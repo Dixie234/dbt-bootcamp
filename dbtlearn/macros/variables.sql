0b04a148dd9d50c43bc4fde1e6e56ec27d2a4b16
Guy Dixon <guy.dixon@creditbenchmark.com>
2025-10-21 16:37:51 +0100

    save variable file

--- dbtlearn/macros/variables.sql
+++ dbtlearn/macros/variables.sql
@@ -2,5 +2,5 @@
     {% set your_name = "zoltan" %}
     {{ log("hello " ~ your_name, info=True) }}
 
-    {{ log("hello dbt user " ~ var("user_name") ~ "!", info=True)}}
+    {{ log("hello dbt user " ~ var("user_name", "NO USERNAME IS SET") ~ "!", info=True)}}
 {% endmacro %}
